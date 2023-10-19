dfb3fa1b0e83717a44d7ea53c3b7de34e36b51ab
harsha16208 <sriharsha16208@gmail.com>
2023-10-19 12:10:23 +0530

    Create InsertAtEndOfDoublyLinkedList.java

---
+++ InsertAtEndOfDoublyLinkedList.java
@@ -0,0 +1,48 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtTail(Node list, int K) {
+        // Write your code here
+        if (list == null) return new Node(K);
+        Node cursor = list;
+        while(cursor.next != null) {
+            cursor = cursor.next;
+        }
+        Node newNode = new Node(K);
+        cursor.next = newNode;
+        newNode.prev = cursor;
+        return list;
+    }
+}
