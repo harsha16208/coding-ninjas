5ead3fc6baa51ac8665099128250e853727e8508
harsha16208 <sriharsha16208@gmail.com>
2023-10-20 11:16:57 +0530

    Create ReverseALinkedList.java

---
+++ ReverseALinkedList.java
@@ -0,0 +1,42 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node reverseLinkedList(Node head)
+    {
+        // Write your code here.
+        Node newNode = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = newNode;
+            newNode = head;
+            head = next;
+        }
+        return newNode;
+    }
+}
