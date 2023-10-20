b3134f11bcf881863ad00ea5443b71739b87adc8
harsha16208 <sriharsha16208@gmail.com>
2023-10-20 11:54:53 +0530

    Create CyclceDetectionInSinglyLinkedLIst.java

---
+++ CyclceDetectionInSinglyLinkedLIst.java
@@ -0,0 +1,45 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+
+    public static boolean detectCycle(Node head) {
+        if (head == null) return false;
+        //Your code goes here
+        Node fast = head;
+        Node slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+
+        return false;
+    }
+}
