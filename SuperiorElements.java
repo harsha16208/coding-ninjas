c6c924e2e50a6d5575370130d2c5c7b869b8664a
harsha_bulusu <sriharsha16208@gmail.com>
2024-02-08 09:48:44 +0530

    Update SuperiorElements.java

--- SuperiorElements.java
+++ SuperiorElements.java
@@ -1,15 +1,20 @@
 import java.util.*;
 public class Solution {
     public static List< Integer > superiorElements(int []a) {
-        List<Integer> list = new ArrayList<>();
-        int max = a[a.length - 1];
-        list.add(a[a.length - 1]);
-        for (int i = a.length - 2; i >= 0; i--) {
+        // Write your code here.
+        //Brute force : Linear search :TC O(N ^ 2); SC: O(n)
+        //optimal
+
+        List<Integer> res = new ArrayList<>();
+        int n = a.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
             if (a[i] > max) {
-                list.add(a[i]);
+                res.add(a[i]);
                 max = a[i];
             }
         }
-        return list;
+
+        return res;
     }
 }
